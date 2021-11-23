509a0f870c07ec7067a39c38a39a94a7308b8c61
koolincn <zhutingsh@hotmail.com>
2021-11-23 23:50:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "ibm_is_security_group_rule" "vsi_sg_rule_out_all" {
 resource "ibm_is_instance" "sample_vsi" {
   depends_on = [ibm_is_security_group_rule.vsi_sg_rule_out_all]
   name           = var.vsi_instance_name
-  image          = local.image_map[var.region]
+  image          = local.imagemap[var.region]
   profile        = data.ibm_is_instance_profile.vsi_profile.name
   resource_group = data.ibm_is_subnet.vsi_subnet.resource_group
 
